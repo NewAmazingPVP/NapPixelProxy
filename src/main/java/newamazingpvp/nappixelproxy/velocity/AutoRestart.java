b84dd93c28aa74dfc88194471cb08541964f4da5
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-12 17:59:20 -0400

    quickly and message velocity integration before 1.21
    quickly and message velocity integration before 1.21

---
+++ src/main/java/newamazingpvp/nappixelproxy/velocity/AutoRestart.java
@@ -0,0 +1,62 @@
+package newamazingpvp.nappixelproxy.velocity;
+
+import com.velocitypowered.api.proxy.ProxyServer;
+
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+public class AutoRestart {
+    private static final long[] warningTimes = {10, 7, 5, 3, 2, 1};
+    private static final String[] warningMessages = {
+            "Proxy will restart in 10 minutes!",
+            "Proxy will restart in 7 minutes!",
+            "Proxy will restart in 5 minutes!",
+            "Proxy will restart in 3 minutes!",
+            "Proxy will restart in 2 minutes!",
+            "Proxy will restart in 1 minute!",
+    };
+
+    public static void scheduleRestart(ProxyServer proxy, NapPixelVelocity plugin) {
+        Calendar now = Calendar.getInstance(TimeZone.getTimeZone("America/New_York"));
+        Calendar restartTime = Calendar.getInstance(TimeZone.getTimeZone("America/New_York"));
+        restartTime.set(Calendar.HOUR_OF_DAY, 3);
+        restartTime.set(Calendar.MINUTE, 0);
+        restartTime.set(Calendar.SECOND, 30);
+
+        if (now.after(restartTime)) {
+            restartTime.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        long initialDelay = restartTime.getTimeInMillis() - now.getTimeInMillis();
+
+        for (int i = 0; i < warningTimes.length; i++) {
+            long warningDelay = initialDelay - (warningTimes[i] * 60 * 1000);
+            scheduleWarning(proxy, plugin, warningDelay, warningMessages[i]);
+        }
+
+        proxy.getScheduler().buildTask(plugin, () -> restartServer(proxy))
+                .delay(Duration.ofMillis(initialDelay))
+                .schedule();
+    }
+
+    private static void scheduleWarning(ProxyServer proxy, NapPixelVelocity plugin, long delay, String message) {
+        proxy.getScheduler().buildTask(plugin, () -> broadcastWarning(proxy, message))
+                .delay(Duration.ofMillis(delay))
+                .schedule();
+    }
+
+    private static void broadcastWarning(ProxyServer proxy, String message) {
+        proxy.getAllPlayers().forEach(player ->
+                player.sendMessage(net.kyori.adventure.text.Component.text(message).color(net.kyori.adventure.text.format.TextColor.color(0xFF0000)))
+        );
+    }
+
+    private static void restartServer(ProxyServer proxy) {
+        proxy.getAllPlayers().forEach(player ->
+                player.sendMessage(net.kyori.adventure.text.Component.text("Restarting the server...").color(net.kyori.adventure.text.format.TextColor.color(0x00FFFF)))
+        );
+        proxy.shutdown();
+    }
+}
---
+++ src/main/java/newamazingpvp/nappixelproxy/velocity/NapPixelVelocity.java
@@ -0,0 +1,121 @@
+package newamazingpvp.nappixelproxy.velocity;
+
+import com.google.inject.Inject;
+import com.moandjiezana.toml.Toml;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.scheduler.ScheduledTask;
+import me.scarsz.jdaappender.ChannelLoggingHandler;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Plugin(id = "nappixelproxy", name = "NapPixelProxy", authors = "NewAmazingPVP")
+public class NapPixelVelocity extends ListenerAdapter {
+
+    private static NapPixelVelocity instance;
+
+    private Toml config;
+    private final ProxyServer proxy;
+    private final Path dataDirectory;
+    private final Set<UUID> messageToggles = new HashSet<>();
+    private Set<String> disabledServers;
+    private Set<String> privateServers;
+    private final ConcurrentHashMap<UUID, String> playerLastServer = new ConcurrentHashMap<>();
+    private final Map<String, String> serverNames = new HashMap<>();
+    public JDA jda;
+    public String consoleChannelId = "1135323447522771114";
+    public ScheduledTask task;
+    public MessageChannel consoleChannel;
+
+    @Inject
+    public NapPixelVelocity(ProxyServer proxy, @DataDirectory Path dataDirectory) {
+        instance = this;
+        this.proxy = proxy;
+        this.dataDirectory = dataDirectory;
+        config = loadConfig(dataDirectory);
+    }
+
+    public static NapPixelVelocity getInstance() {
+        return instance;
+    }
+
+    @Subscribe
+    public void onProxyInitialize(ProxyInitializeEvent event) {
+        config = loadConfig(dataDirectory);
+        initializeVelocityBot();
+        AutoRestart.scheduleRestart(proxy, this);
+    }
+
+    private Toml loadConfig(Path path) {
+        File folder = path.toFile();
+        File file = new File(folder, "config.toml");
+
+        if (!file.exists()) {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            try (InputStream input = getClass().getResourceAsStream("/" + file.getName())) {
+                if (input != null) {
+                    Files.copy(input, file.toPath());
+                } else {
+                    file.createNewFile();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+                return null;
+            }
+        }
+        return new Toml().read(file);
+    }
+
+    public void initializeVelocityBot() {
+        String token = config.getString("BotToken");
+        EnumSet<GatewayIntent> allIntents = EnumSet.allOf(GatewayIntent.class);
+
+        JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+        jdaBuilder.enableIntents(allIntents);
+        jda = jdaBuilder.build();
+        task = proxy.getScheduler().buildTask(this, () -> {
+            consoleChannel = jda.getTextChannelById(consoleChannelId);
+            if (consoleChannel != null) {
+                ChannelLoggingHandler handler1 = new ChannelLoggingHandler(() -> consoleChannel, config -> {
+                    config.setColored(true);
+                    config.setSplitCodeBlockForLinks(false);
+                    config.setAllowLinkEmbeds(true);
+                    config.mapLoggerName("net.dv8tion.jda", "JDA");
+                    config.mapLoggerName("net.minecraft.server.MinecraftServer", "Server");
+                    config.mapLoggerNameFriendly("net.minecraft.server", s -> "Server/" + s);
+                    config.mapLoggerNameFriendly("net.minecraft", s -> "Minecraft/" + s);
+                    config.mapLoggerName("github.scarsz.discordsrv.dependencies.jda", s -> "DiscordSRV/JDA/" + s);
+                }).attach();
+                handler1.schedule();
+                task.cancel();
+            }
+        }).delay(Duration.ofSeconds(1)).repeat(Duration.ofSeconds(1)).schedule();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String bungeeChannelId = "1135323447522771114";
+        if (event.getMessage().getChannelId().equals(bungeeChannelId)) {
+            String messageContent = event.getMessage().getContentRaw();
+            proxy.getCommandManager().executeImmediatelyAsync(proxy.getConsoleCommandSource(), messageContent);
+        }
+    }
+}
