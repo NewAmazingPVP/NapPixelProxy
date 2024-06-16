c2042180f308ab44081a211b8cf46c6992ad9dbf
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-16 13:58:02 -0400

    hackfix for reconnect plugin
    hackfix for reconnect plugin

--- src/main/java/newamazingpvp/nappixelproxy/velocity/NapPixelVelocity.java
+++ src/main/java/newamazingpvp/nappixelproxy/velocity/NapPixelVelocity.java
@@ -2,11 +2,20 @@ package newamazingpvp.nappixelproxy.velocity;
 
 import com.google.inject.Inject;
 import com.moandjiezana.toml.Toml;
+import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.player.KickedFromServerEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.ServerConnection;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import com.velocitypowered.api.proxy.server.ServerInfo;
 import com.velocitypowered.api.scheduler.ScheduledTask;
 import me.scarsz.jdaappender.ChannelLoggingHandler;
 import net.dv8tion.jda.api.JDA;
@@ -15,15 +24,20 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.title.Title;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Plugin(id = "nappixelproxy", name = "NapPixelProxy", authors = "NewAmazingPVP")
 public class NapPixelVelocity extends ListenerAdapter {
@@ -60,6 +74,7 @@ public class NapPixelVelocity extends ListenerAdapter {
         config = loadConfig(dataDirectory);
         initializeVelocityBot();
         AutoRestart.scheduleRestart(proxy, this);
+        createLimboServer();
     }
 
     private Toml loadConfig(Path path) {
@@ -99,6 +114,10 @@ public class NapPixelVelocity extends ListenerAdapter {
                     config.setColored(true);
                     config.setSplitCodeBlockForLinks(false);
                     config.setAllowLinkEmbeds(true);
+                    config.addFilter(logItem -> {
+                        String message = logItem.getMessage();
+                        return !message.contains("unable to connect");
+                    });
                     config.mapLoggerName("net.dv8tion.jda", "JDA");
                     config.mapLoggerName("net.minecraft.server.MinecraftServer", "Server");
                     config.mapLoggerNameFriendly("net.minecraft.server", s -> "Server/" + s);
@@ -118,5 +137,40 @@ public class NapPixelVelocity extends ListenerAdapter {
             String messageContent = event.getMessage().getContentRaw();
             proxy.getCommandManager().executeImmediatelyAsync(proxy.getConsoleCommandSource(), messageContent);
         }
+    }
+
+    private void createLimboServer() {
+        ServerInfo limboServerInfo = new ServerInfo("limbo", new InetSocketAddress("0.0.0.0", 14144));
+        proxy.registerServer(limboServerInfo);
+    }
+
+    @Subscribe
+    public void onPlayerKicked(KickedFromServerEvent event) {
+        Player player = event.getPlayer();
+
+        Optional<RegisteredServer> limboServer = proxy.getServer("limbo");
+        if (limboServer.isPresent()) {
+            event.setResult(KickedFromServerEvent.RedirectPlayer.create(limboServer.get(), Component.text("Server is restarting. Please wait...")));
+            keepPlayerInLimbo(player, limboServer.get());
+        }
+    }
+
+
+    private void keepPlayerInLimbo(Player player, RegisteredServer limboServer) {
+        proxy.getScheduler().buildTask(this, () -> {
+            player.showTitle(Title.title(
+                    Component.text("Server Background Restarting")
+                            .color(NamedTextColor.RED),
+                    Component.text("Please stay connected...")
+                            .color(NamedTextColor.YELLOW)
+            ));
+
+            player.sendActionBar(Component.text("Server background restarting. Please stay connected...")
+                    .color(NamedTextColor.YELLOW));
+
+            player.sendMessage(Component.text("Server background restarting. Please stay connected...")
+                    .color(NamedTextColor.YELLOW));
+
+        }).repeat(10, TimeUnit.SECONDS).schedule();
     }
 }
